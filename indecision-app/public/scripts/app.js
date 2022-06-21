a0ce37badafa13006613c46c5ff8a368cd970e74
alvarosd <a.sanchez@andyor.com>
2022-06-21 14:21:35 +0200

    Install babel

--- indecision-app/public/scripts/app.js
+++
@@ -1,9 +0,0 @@
-console.log('App.js is running!!')
-
-
-//JSX -JavaScript XML
-//var template = < p > This is JSX from app.js!! < /p>;
-//using babeljs the above code translate into the following
-var template = /*#__PURE__*/ React.createElement("p", null, " This is JSX from app.js!! ");
-var appRoute = document.getElementById('app')
-ReactDOM.render(template, appRoute);
---
+++ indecision-app/src/app.js
@@ -0,0 +1,7 @@
+console.log('App.js is running!!')
+
+
+//JSX -JavaScript XML
+var template = < h1 > Does this change ? < /h1> <p> Does this change ? </p >
+    var appRoute = document.getElementById('app')
+ReactDOM.render(template, appRoute);
