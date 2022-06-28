004279807d7b382de06aa4e71a11ee89ca0d494e
AlvaroSanchezDomingo <alvaro.sanchez.domingo@gmail.com>
2022-06-28 19:03:25 +0200

    Fix "env,react" in babel comand in home laptop

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,37 +1,35 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var show = false;
+var toggle = function toggle() {
+    show = !show;
+    console.log(show);
+    render();
+};
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var render = function render() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h2',
+            null,
+            ' Visibility Toggle'
+        ),
+        React.createElement(
+            'button',
+            { onClick: toggle },
+            show ? 'Hide Details' : 'Show Details'
+        ),
+        show && React.createElement(
+            'p',
+            null,
+            'Here are some details!!'
+        )
+    );
+    ReactDOM.render(template, appRoute);
+};
 
-var Person = function () {
-    function Person() {
-        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
-        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+var appRoute = document.getElementById('app');
 
-        _classCallCheck(this, Person);
-
-        this.name = name;
-        this.age = age;
-    }
-
-    _createClass(Person, [{
-        key: 'getGretting',
-        value: function getGretting() {
-            return 'Hi. I am ' + this.name + '!';
-        }
-    }, {
-        key: 'getDescription',
-        value: function getDescription() {
-            return this.name + ' is ' + this.age + ' year(s) old!';
-        }
-    }]);
-
-    return Person;
-}();
-
-var me = new Person('Andrew Mead', 27);
-console.log(me.getDescription());
-
-var other = new Person();
-console.log(other.getDescription());
+render();
