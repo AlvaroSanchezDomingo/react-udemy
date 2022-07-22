8f66780476f20808938ac2d680f964b15ef89f0e
alvarosd <a.sanchez@andyor.com>
2022-07-22 15:00:40 +0200

    HOC

---
+++ expensify-app/src/playground/hoc.js
@@ -0,0 +1,29 @@
+//Higher Order Componen HOC - A component HOC that renders another component
+//Reuse code
+//Render hijacking
+//Props manipulaton
+//Abstract state
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const Info = (props) => (
+    <div>
+        <h1>Info</h1>
+        <p>The info is {props.info}</p>
+    </div>
+)
+
+const withAdminWarning = (WrappedComponnent) =>{
+    return (props) =>(
+        <div>
+            {props.isAdmin && <p>This is private info. Please don't share</p>}
+            <WrappedComponnent {...props}/>
+        </div>
+    )
+}
+
+const AdminInfo = withAdminWarning(info)
+ReactDOM.render(<AdminInfo isAdmin = {true} info = 'This is the info'/>)
