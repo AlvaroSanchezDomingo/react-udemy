488552af32ca04d5d045f3fdffb52e0f0a88dc19
alvarosd <a.sanchez@andyor.com>
2022-08-18 15:36:53 +0200

    more advance jest test in components

--- expensify-app/src/tests/components/Header.test.js
+++ expensify-app/src/tests/components/Header.test.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import {shallow} from 'enzyme'
-//import toJSON from 'enzyme-to-json'//Is in the config file
 import Header from '../../components/Header'
 
 
 test('should render Header correctly', () =>{
-    // const renderer = new ReactShallowRenderer()
-    // renderer.render(<Header/>)
-    // expect(renderer.getRenderOutput()).toMatchSnapshot()
-    // console.log(renderer.getRenderOutput())
-    // expect(wrapper.find('h1').length.toBe(1))//we will conpare with snapshot
-    // expect(toJSON(wrapper)).toMatchSnapshot()//is in the config file
     const wrapper = shallow(<Header/>)
     expect(wrapper).toMatchSnapshot()
-   
 })
+
